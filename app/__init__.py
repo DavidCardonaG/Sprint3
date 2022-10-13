01a8da8d752cf393df6939551d5e78c188c2ca1d
DavidCardonaG <softwarewd@outlook.com>
2022-10-12 22:15:02 -0500

    app dkj

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,12 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY = dbc.SEC,
-        DATABASE = os.path.join(app.instance_path, 'app.sqlite'),
+        DATABASE = os.path.join(app.instance_path, "app.sqlite"),
     )
 
     if test_config is None:
         # Load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # Load the test config if passed in
         app.config.from_mapping(test_config)
